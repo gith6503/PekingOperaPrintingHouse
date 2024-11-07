e6931bdfb99dfeff81e5f6fcb1e3877974bed071
opera <opera@LAPTOP-BN3KN4RT>
2024-11-07 12:30:57 +0800

    initial commit

---
+++ services/create.php
@@ -0,0 +1,72 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  opera
+ * Created: 17 Oct 2024
+ */
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  
+// get database connection
+include_once '../config/database.php';
+  
+// instantiate product object
+include_once '../objects/services.php';
+  
+$database = new Database();
+$db = $database->getConnection();
+  
+$services = new services($db);
+  
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+  
+// make sure data is not empty
+if(
+    !empty($data->service_no) &&
+    !empty($data->service_name)
+){
+  
+    // set product property values
+    $services->service_no = $data->service_no;
+    $services->service_name = $data->service_name;
+  
+  
+    // create the product
+    if($services->create()){
+  
+        // set response code - 201 created
+        http_response_code(201);
+  
+        // tell the user
+        echo json_encode(array("message" => "Services was created."));
+    }
+  
+    // if unable to create the product, tell the user
+    else{
+  
+        // set response code - 503 service unavailable
+        http_response_code(503);
+  
+        // tell the user
+        echo json_encode(array("message" => "Unable to create services."));
+    }
+}
+  
+// tell the user data is incomplete
+else{
+  
+    // set response code - 400 bad request
+    http_response_code(400);
+  
+    // tell the user
+    echo json_encode(array("message" => "Unable to create product. Data is incomplete."));
+}
+?>
