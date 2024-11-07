25f538b4c00cf79350a85eedf5d7fdada1d5d85c
opera <opera@LAPTOP-BN3KN4RT>
2024-11-07 14:06:04 +0800

    change commit

--- services/update.php
+++ services/update.php
@@ -1,11 +1,3 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
- */
-/**
- * Author:  opera
- * Created: 17 Oct 2024
- */
 <?php
 // required headers
 header("Access-Control-Allow-Origin: *");
@@ -13,45 +5,44 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-  
+
 // include database and object files
 include_once '../config/database.php';
 include_once '../objects/services.php';
-  
+
 // get database connection
 $database = new Database();
 $db = $database->getConnection();
-  
-// prepare product object
-$services = new services($db);
-  
-// get id of product to be edited
+
+// prepare services object
+$services = new Services($db);
+
+// get the posted data
 $data = json_decode(file_get_contents("php://input"));
-  
-// set ID property of product to be edited
-$services->service_no = $data->service_no;
-  
-// set product property values
-$services->service_no = $data->service_no;
-$services->service_name = $data->service_name;
-  
-// update the product
-if($services->update()){
-  
-    // set response code - 200 ok
-    http_response_code(200);
-  
+
+// ensure the required fields are set
+if (!empty($data->service_no) && !empty($data->service_name)) {
+
+    // set the ID and properties of the service to be updated
+    $services->service_no = $data->service_no;
+    $services->service_name = $data->service_name;
+
+    // update the service
+    if ($services->update()) {
+        // set response code - 200 OK
+        http_response_code(200);
+        // tell the user
+        echo json_encode(array("message" => "Service was updated."));
+    } else {
+        // set response code - 503 service unavailable
+        http_response_code(503);
+        // tell the user
+        echo json_encode(array("message" => "Unable to update service."));
+    }
+} else {
+    // set response code - 400 Bad Request
+    http_response_code(400);
     // tell the user
-    echo json_encode(array("message" => "Service was updated."));
-}
-  
-// if unable to update the product, tell the user
-else{
-  
-    // set response code - 503 service unavailable
-    http_response_code(503);
-  
-    // tell the user
-    echo json_encode(array("message" => "Unable to update product."));
+    echo json_encode(array("message" => "Unable to update service. Data is incomplete."));
 }
 ?>
