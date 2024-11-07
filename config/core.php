d112c7306012ae21daefdd521209f0a6d292effd
opera <opera@LAPTOP-BN3KN4RT>
2024-11-07 13:38:42 +0800

    change commit

---
+++ config/core.php
@@ -0,0 +1,25 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  opera
+ * Created: 17 Oct 2024
+ */
+<?php
+// show error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+  
+// home page url
+$home_url="http://localhost/api/";
+  
+// page given in URL parameter, default page is one
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+  
+// set number of records per page
+$records_per_page = 5;
+  
+// calculate for the query LIMIT clause
+$from_record_num = ($records_per_page * $page) - $records_per_page;
+?>
