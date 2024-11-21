96c4c273398579c1da392361e3a5c66dd739e270
opera <opera@LAPTOP-BN3KN4RT>
2024-11-21 20:36:06 +0800

    change commit

---
+++ orders/read.php
@@ -0,0 +1,56 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/orders.php';
+
+// instantiate database and orders object
+$database = new Database();
+$db = $database->getConnection();
+
+// initialize object
+$orders = new Orders($db);
+
+// query orders
+$stmt = $orders->read();
+$num = $stmt->rowCount();
+
+// check if more than 0 records found
+if($num > 0){
+
+    // orders array
+    $orders_arr = array();
+    $orders_arr["records"] = array();
+
+    // retrieve table contents
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+
+        $order_item = array(
+            "job_no" => $job_no,
+            "customer_no" => $customer_no,
+            "customer_name" => $customer_name,
+            "job_status" => $job_status,
+            "order_status" => $order_status,
+            "estimated_completion" => $estimated_completion,
+        );
+
+        array_push($orders_arr["records"], $order_item);
+    }
+
+    // set response code - 200 OK
+    http_response_code(200);
+
+    // show orders data in JSON format
+    echo json_encode($orders_arr);
+} else {
+    // set response code - 404 Not found
+    http_response_code(404);
+
+    // tell the user no orders found
+    echo json_encode(array("message" => "No orders found."));
+}
+?>
