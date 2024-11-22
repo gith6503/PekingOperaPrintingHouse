21f74c905ed870f19bfb73c8fcd9ad9f83e3b2a9
opera <opera@LAPTOP-BN3KN4RT>
2024-11-22 10:25:49 +0800

    change commit

--- services/read.php
+++ services/read.php
@@ -17,14 +17,14 @@ header("Content-Type: application/json; charset=UTF-8");
 include_once '../config/database.php';
 include_once '../objects/services.php';
 
-// instantiate database and product object
+// instantiate database and services object
 $database = new Database();
 $db = $database->getConnection();
 
 // initialize object
 $services = new Services($db);
 
-// query products
+// query services
 $stmt = $services->read();
 $num = $stmt->rowCount();
 
@@ -50,14 +50,14 @@ if($num > 0){
     // set response code - 200 OK
     http_response_code(200);
 
-    // show products data in JSON format
+    // show services data in JSON format
     echo json_encode($services_arr);
 }
 else {
     // set response code - 404 Not found
     http_response_code(404);
 
-    // tell the user no products found
+    // tell the user no services found
     echo json_encode(array("message" => "No services found."));
 }
 ?>
